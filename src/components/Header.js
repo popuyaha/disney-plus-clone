f0b0b95f1f9605f373fb0d2a2db7a37c7fc92126
popuyaha <popuyaha@Naver.com>
2021-11-01 16:58:35 +0900

    Revert "헤더 세팅 & 이미지 세팅"
    This reverts commit b83c651e2c4fe9073771e29eac32f173eb74cbbb.

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,7 +87,7 @@ const NavMenu = styled.div`
                 bottom: -2px;
                 opacity: 0;
                 transform-origin: left center;
-                transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+                transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94)
                 transform: scaleX(0);
             }
         }
